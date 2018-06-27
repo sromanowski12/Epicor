c685e703a3cafb9c2720a1fc341fc0612d5e6930
Seth Romanowski <sromanowski12@gmail.com>
2018-06-27 15:35:59 -0400

    changed BAQ parameter

---
+++ Epicor/Epicor/Form1.Designer.cs
@@ -0,0 +1,154 @@
+﻿namespace Epicor
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.button1 = new System.Windows.Forms.Button();
+            this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            this.comboBox1 = new System.Windows.Forms.ComboBox();
+            this.menuStrip1 = new System.Windows.Forms.MenuStrip();
+            this.fileToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.optionsToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.resourcesToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.exitToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
+            this.menuStrip1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(466, 47);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "button1";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // dataGridView1
+            // 
+            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView1.Location = new System.Drawing.Point(11, 77);
+            this.dataGridView1.Name = "dataGridView1";
+            this.dataGridView1.RowTemplate.Height = 24;
+            this.dataGridView1.Size = new System.Drawing.Size(1077, 432);
+            this.dataGridView1.TabIndex = 1;
+            // 
+            // comboBox1
+            // 
+            this.comboBox1.FormattingEnabled = true;
+            this.comboBox1.Items.AddRange(new object[] {
+            "Test",
+            "CustOrdHist"});
+            this.comboBox1.Location = new System.Drawing.Point(11, 48);
+            this.comboBox1.Name = "comboBox1";
+            this.comboBox1.Size = new System.Drawing.Size(318, 24);
+            this.comboBox1.TabIndex = 2;
+            // 
+            // menuStrip1
+            // 
+            this.menuStrip1.ImageScalingSize = new System.Drawing.Size(20, 20);
+            this.menuStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.fileToolStripMenuItem,
+            this.optionsToolStripMenuItem,
+            this.resourcesToolStripMenuItem});
+            this.menuStrip1.Location = new System.Drawing.Point(0, 0);
+            this.menuStrip1.Name = "menuStrip1";
+            this.menuStrip1.Size = new System.Drawing.Size(1100, 28);
+            this.menuStrip1.TabIndex = 3;
+            this.menuStrip1.Text = "menuStrip1";
+            // 
+            // fileToolStripMenuItem
+            // 
+            this.fileToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.exitToolStripMenuItem});
+            this.fileToolStripMenuItem.Name = "fileToolStripMenuItem";
+            this.fileToolStripMenuItem.Size = new System.Drawing.Size(44, 24);
+            this.fileToolStripMenuItem.Text = "File";
+            // 
+            // optionsToolStripMenuItem
+            // 
+            this.optionsToolStripMenuItem.Name = "optionsToolStripMenuItem";
+            this.optionsToolStripMenuItem.Size = new System.Drawing.Size(73, 24);
+            this.optionsToolStripMenuItem.Text = "Options";
+            // 
+            // resourcesToolStripMenuItem
+            // 
+            this.resourcesToolStripMenuItem.Name = "resourcesToolStripMenuItem";
+            this.resourcesToolStripMenuItem.Size = new System.Drawing.Size(87, 24);
+            this.resourcesToolStripMenuItem.Text = "Resources";
+            // 
+            // exitToolStripMenuItem
+            // 
+            this.exitToolStripMenuItem.Name = "exitToolStripMenuItem";
+            this.exitToolStripMenuItem.Size = new System.Drawing.Size(216, 26);
+            this.exitToolStripMenuItem.Text = "Exit";
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(360, 48);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(100, 22);
+            this.textBox1.TabIndex = 4;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1100, 521);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.comboBox1);
+            this.Controls.Add(this.dataGridView1);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.menuStrip1);
+            this.MainMenuStrip = this.menuStrip1;
+            this.Name = "Form1";
+            this.Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
+            this.menuStrip1.ResumeLayout(false);
+            this.menuStrip1.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.DataGridView dataGridView1;
+        private System.Windows.Forms.ComboBox comboBox1;
+        private System.Windows.Forms.MenuStrip menuStrip1;
+        private System.Windows.Forms.ToolStripMenuItem fileToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem exitToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem optionsToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem resourcesToolStripMenuItem;
+        private System.Windows.Forms.TextBox textBox1;
+    }
+}
+
---
+++ Epicor/Epicor/Form1.cs
@@ -0,0 +1,162 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.IO;
+using Erp.BO;
+using Ice.Core;
+using Ice.Lib.Framework;
+using Ice.Proxy.BO;
+using Erp;
+using Epicor.Hosting;
+using Epicor.Utilities;
+using Ice.BO.DynamicQuery;
+using System.Runtime;
+using System.Windows;
+
+
+namespace Epicor
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            Session epiSession;
+            epiSession = new Session("seth.romanowski", "Baseball33", "net.tcp://LW-EPICOR-APP/ERP102100", Session.LicenseType.Default, @"C:\Epicor\ERP10.2Client2\Client\config\ERP102100.sysconfig");
+            ILauncher oTrans = null;
+            if (epiSession != null)
+            {
+                MessageBox.Show("Connected!");
+                oTrans = new ILauncher(epiSession);
+            }
+
+            try
+            {
+                if (comboBox1.Text == "Test")
+                {
+                    // Declare and Initialize Variables
+                    string BAQName = "Test";
+                    Ice.BO.QueryExecutionDataSet ds = new Ice.BO.QueryExecutionDataSet();
+
+                    // Add Parameter Rows
+                    // Definition: AddExecutionParameterRow(string ParameterID, string ParameterValue, string ValueType, bool IsEmpty, Guid SysRowID, string RowMod)
+                    // Possible ValueTypes: nvarchar, int, decimal, date, datetime, bit, uniqueidentifier, bigint
+                    // IsEmpty indicates if your passed value Is Empty because if it is, you can define in your params to use a default value if empty.
+                    // Typically you use string.IsNullOrEmpty(yourValueVariable) but if you are hard coding a value then you can simply set it to false
+                    ds.ExecutionParameter.AddExecutionParameterRow("PartNum", "1040980", "nvarchar", false, Guid.Empty, "A");
+
+                    // Use Business Object Directly
+                    Ice.Proxy.BO.DynamicQueryImpl dynamicQuery = WCFServiceSupport.CreateImpl<Ice.Proxy.BO.DynamicQueryImpl>((Ice.Core.Session)oTrans.Session, Epicor.ServiceModel.Channels.ImplBase<Ice.Contracts.DynamicQuerySvcContract>.UriPath);
+                    System.Data.DataSet results = dynamicQuery.ExecuteByID(BAQName, ds);
+
+                    string val, val2, val3, val4, val5, val6;
+                    string[] row;
+
+                    dataGridView1.ColumnCount = 6;
+                    dataGridView1.Columns[0].Name = "JobNum";
+                    dataGridView1.Columns[1].Name = "StartDate";
+                    dataGridView1.Columns[2].Name = "DueDate";
+                    dataGridView1.Columns[3].Name = "PartNum";
+                    dataGridView1.Columns[4].Name = "PartDesc";
+                    dataGridView1.Columns[5].Name = "ProdQty";
+
+
+                    // Lets Loop through our results
+                    if (results.Tables["Results"].Rows.Count > 0)
+                    {
+                        foreach (DataRow item in results.Tables["Results"].Rows)
+                        {
+                            // In E9 you used TableName.Column in E10 it is TableName_Column
+                            val = item["JobHead_JobNum"].ToString();
+                            val2 = item["JobHead_StartDate"].ToString();
+                            val3 = item["JobHead_DueDate"].ToString();
+                            val4 = item["JobHead_PartNum"].ToString();
+                            val5 = item["JobHead_PartDescription"].ToString();
+                            val6 = item["JobHead_ProdQty"].ToString();
+
+                            row = new string[] { val, val2, val3, val4, val5, val6 };
+                            dataGridView1.Rows.Add(row);
+                        }
+                    }
+                }
+                if (comboBox1.Text == "CustOrdHist")
+                {
+                    // Declare and Initialize Variables
+                    string BAQName = "CustOrdHist";
+                    Ice.BO.QueryExecutionDataSet ds = new Ice.BO.QueryExecutionDataSet();
+
+                    // Add Parameter Rows
+                    // Definition: AddExecutionParameterRow(string ParameterID, string ParameterValue, string ValueType, bool IsEmpty, Guid SysRowID, string RowMod)
+                    // Possible ValueTypes: nvarchar, int, decimal, date, datetime, bit, uniqueidentifier, bigint
+                    // IsEmpty indicates if your passed value Is Empty because if it is, you can define in your params to use a default value if empty.
+                    // Typically you use string.IsNullOrEmpty(yourValueVariable) but if you are hard coding a value then you can simply set it to false
+                    ds.ExecutionParameter.AddExecutionParameterRow("CustID", textBox1.Text.ToString(), "nvarchar", false, Guid.Empty, "A");
+
+                    // Use Business Object Directly
+                    Ice.Proxy.BO.DynamicQueryImpl dynamicQuery = WCFServiceSupport.CreateImpl<Ice.Proxy.BO.DynamicQueryImpl>((Ice.Core.Session)oTrans.Session, Epicor.ServiceModel.Channels.ImplBase<Ice.Contracts.DynamicQuerySvcContract>.UriPath);
+                    System.Data.DataSet results = dynamicQuery.ExecuteByID(BAQName, ds);
+
+                    string val, val2, val3, val4, val5, val6, val7, val8, val9, val10, val11, val12, val13;
+                    string[] row;
+
+                    dataGridView1.ColumnCount = 13;
+                    dataGridView1.Columns[0].Name = "Name";
+                    dataGridView1.Columns[1].Name = "Order";
+                    dataGridView1.Columns[2].Name = "OrderDate";
+                    dataGridView1.Columns[3].Name = "NeedBy";
+                    dataGridView1.Columns[4].Name = "Line";
+                    dataGridView1.Columns[5].Name = "NeedBy";
+                    dataGridView1.Columns[6].Name = "Part";
+                    dataGridView1.Columns[7].Name = "Part";
+                    dataGridView1.Columns[8].Name = "Rev";
+                    dataGridView1.Columns[9].Name = "Desc";
+                    dataGridView1.Columns[10].Name = "UnitPrice";
+                    dataGridView1.Columns[11].Name = "OrderQty";
+                    dataGridView1.Columns[12].Name = "DocUnitPrice";
+
+                    // Lets Loop through our results
+                    if (results.Tables["Results"].Rows.Count > 0)
+                    {
+                        foreach (DataRow item in results.Tables["Results"].Rows)
+                        {
+                            // In E9 you used TableName.Column in E10 it is TableName_Column
+                            val = item["Customer_Name"].ToString();
+                            val2 = item["OrderHed_OrderNum"].ToString();
+                            val3 = item["OrderHed_OrderDate"].ToString();
+                            val4 = item["OrderHed_NeedByDate"].ToString();
+                            val5 = item["OrderDtl_OrderLine"].ToString();
+                            val6 = item["OrderDtl_NeedByDate"].ToString();
+                            val7 = item["OrderDtl_PartNum"].ToString();
+                            val8 = item["OrderDtl_XPartNum"].ToString();
+                            val9 = item["PartRev_RevisionNum"].ToString();
+                            val10 = item["Part_PartDescription"].ToString();
+                            val11 = item["OrderDtl_UnitPrice"].ToString();
+                            val12 = item["OrderDtl_OrderQty"].ToString();
+                            val13 = item["OrderDtl_DocUnitPrice"].ToString();
+
+                            row = new string[] { val, val2, val3, val4, val5, val6, val7, val8, val9, val10, val11, val12, val13 };
+                            dataGridView1.Rows.Add(row);
+                        }
+                    }
+                }
+            }
+            catch (System.Exception ex)
+            {
+                MessageBox.Show(ex.ToString());
+            }
+       
+        }
+
+
+    }
+}
