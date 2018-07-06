9558fdf88a2d653bbb5209849d0b1524493dbce1
Seth Romanowski <sromanowski12@gmail.com>
2018-07-06 15:25:45 -0400

    Changed to Epicor 10.2.2 and added PartMtlOnHand2

--- Epicor/Epicor/Form1.Designer.cs
+++ Epicor/Epicor/Form1.Designer.cs
@@ -28,22 +28,46 @@
         /// </summary>
         private void InitializeComponent()
         {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
             this.button1 = new System.Windows.Forms.Button();
             this.dataGridView1 = new System.Windows.Forms.DataGridView();
             this.comboBox1 = new System.Windows.Forms.ComboBox();
             this.menuStrip1 = new System.Windows.Forms.MenuStrip();
             this.fileToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.exitToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
             this.optionsToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
             this.resourcesToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
-            this.exitToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
             this.textBox1 = new System.Windows.Forms.TextBox();
+            this.exportToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.excelToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.sendToToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.newTaskToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.saveToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.saveAsToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.exitToolStripMenuItem1 = new System.Windows.Forms.ToolStripMenuItem();
+            this.printPreviewToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.exitToolStripMenuItem2 = new System.Windows.Forms.ToolStripMenuItem();
+            this.outlookToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.newEmailToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.newTaskToolStripMenuItem2 = new System.Windows.Forms.ToolStripMenuItem();
+            this.iNSERTToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.uPDATEToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.dELETEToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.dateTimePicker1 = new System.Windows.Forms.DateTimePicker();
+            this.dateTimePicker2 = new System.Windows.Forms.DateTimePicker();
+            this.label3 = new System.Windows.Forms.Label();
             ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
             this.menuStrip1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
             this.SuspendLayout();
             // 
             // button1
             // 
-            this.button1.Location = new System.Drawing.Point(466, 47);
+            this.button1.Location = new System.Drawing.Point(11, 86);
             this.button1.Name = "button1";
             this.button1.Size = new System.Drawing.Size(75, 23);
             this.button1.TabIndex = 0;
@@ -54,10 +78,10 @@
             // dataGridView1
             // 
             this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
-            this.dataGridView1.Location = new System.Drawing.Point(11, 77);
+            this.dataGridView1.Location = new System.Drawing.Point(11, 115);
             this.dataGridView1.Name = "dataGridView1";
             this.dataGridView1.RowTemplate.Height = 24;
-            this.dataGridView1.Size = new System.Drawing.Size(1077, 432);
+            this.dataGridView1.Size = new System.Drawing.Size(1077, 394);
             this.dataGridView1.TabIndex = 1;
             // 
             // comboBox1
@@ -65,10 +89,11 @@
             this.comboBox1.FormattingEnabled = true;
             this.comboBox1.Items.AddRange(new object[] {
             "Test",
-            "CustOrdHist"});
-            this.comboBox1.Location = new System.Drawing.Point(11, 48);
+            "CustOrdHist",
+            "Part Material On-Hand"});
+            this.comboBox1.Location = new System.Drawing.Point(116, 50);
             this.comboBox1.Name = "comboBox1";
-            this.comboBox1.Size = new System.Drawing.Size(318, 24);
+            this.comboBox1.Size = new System.Drawing.Size(186, 24);
             this.comboBox1.TabIndex = 2;
             // 
             // menuStrip1
@@ -87,10 +112,21 @@
             // fileToolStripMenuItem
             // 
             this.fileToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
-            this.exitToolStripMenuItem});
+            this.exitToolStripMenuItem,
+            this.saveToolStripMenuItem,
+            this.saveAsToolStripMenuItem,
+            this.exitToolStripMenuItem1,
+            this.printPreviewToolStripMenuItem,
+            this.exitToolStripMenuItem2});
             this.fileToolStripMenuItem.Name = "fileToolStripMenuItem";
             this.fileToolStripMenuItem.Size = new System.Drawing.Size(44, 24);
             this.fileToolStripMenuItem.Text = "File";
+            // 
+            // exitToolStripMenuItem
+            // 
+            this.exitToolStripMenuItem.Name = "exitToolStripMenuItem";
+            this.exitToolStripMenuItem.Size = new System.Drawing.Size(216, 26);
+            this.exitToolStripMenuItem.Text = "Open";
             // 
             // optionsToolStripMenuItem
             // 
@@ -100,28 +136,193 @@
             // 
             // resourcesToolStripMenuItem
             // 
+            this.resourcesToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.exportToolStripMenuItem,
+            this.sendToToolStripMenuItem,
+            this.outlookToolStripMenuItem});
             this.resourcesToolStripMenuItem.Name = "resourcesToolStripMenuItem";
             this.resourcesToolStripMenuItem.Size = new System.Drawing.Size(87, 24);
             this.resourcesToolStripMenuItem.Text = "Resources";
             // 
-            // exitToolStripMenuItem
-            // 
-            this.exitToolStripMenuItem.Name = "exitToolStripMenuItem";
-            this.exitToolStripMenuItem.Size = new System.Drawing.Size(216, 26);
-            this.exitToolStripMenuItem.Text = "Exit";
-            // 
             // textBox1
             // 
-            this.textBox1.Location = new System.Drawing.Point(360, 48);
+            this.textBox1.Location = new System.Drawing.Point(381, 48);
             this.textBox1.Name = "textBox1";
             this.textBox1.Size = new System.Drawing.Size(100, 22);
             this.textBox1.TabIndex = 4;
+            // 
+            // exportToolStripMenuItem
+            // 
+            this.exportToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.excelToolStripMenuItem,
+            this.iNSERTToolStripMenuItem,
+            this.uPDATEToolStripMenuItem,
+            this.dELETEToolStripMenuItem});
+            this.exportToolStripMenuItem.Name = "exportToolStripMenuItem";
+            this.exportToolStripMenuItem.Size = new System.Drawing.Size(216, 26);
+            this.exportToolStripMenuItem.Text = "Epicor";
+            // 
+            // excelToolStripMenuItem
+            // 
+            this.excelToolStripMenuItem.Name = "excelToolStripMenuItem";
+            this.excelToolStripMenuItem.Size = new System.Drawing.Size(216, 26);
+            this.excelToolStripMenuItem.Text = "SELECT";
+            // 
+            // sendToToolStripMenuItem
+            // 
+            this.sendToToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.newTaskToolStripMenuItem});
+            this.sendToToolStripMenuItem.Name = "sendToToolStripMenuItem";
+            this.sendToToolStripMenuItem.Size = new System.Drawing.Size(216, 26);
+            this.sendToToolStripMenuItem.Text = "Excel";
+            // 
+            // newTaskToolStripMenuItem
+            // 
+            this.newTaskToolStripMenuItem.Name = "newTaskToolStripMenuItem";
+            this.newTaskToolStripMenuItem.Size = new System.Drawing.Size(216, 26);
+            this.newTaskToolStripMenuItem.Text = "New";
+            // 
+            // saveToolStripMenuItem
+            // 
+            this.saveToolStripMenuItem.Name = "saveToolStripMenuItem";
+            this.saveToolStripMenuItem.Size = new System.Drawing.Size(216, 26);
+            this.saveToolStripMenuItem.Text = "Save";
+            // 
+            // saveAsToolStripMenuItem
+            // 
+            this.saveAsToolStripMenuItem.Name = "saveAsToolStripMenuItem";
+            this.saveAsToolStripMenuItem.Size = new System.Drawing.Size(216, 26);
+            this.saveAsToolStripMenuItem.Text = "Save As";
+            // 
+            // exitToolStripMenuItem1
+            // 
+            this.exitToolStripMenuItem1.Name = "exitToolStripMenuItem1";
+            this.exitToolStripMenuItem1.Size = new System.Drawing.Size(216, 26);
+            this.exitToolStripMenuItem1.Text = "Print";
+            // 
+            // printPreviewToolStripMenuItem
+            // 
+            this.printPreviewToolStripMenuItem.Name = "printPreviewToolStripMenuItem";
+            this.printPreviewToolStripMenuItem.Size = new System.Drawing.Size(216, 26);
+            this.printPreviewToolStripMenuItem.Text = "Print Preview";
+            // 
+            // exitToolStripMenuItem2
+            // 
+            this.exitToolStripMenuItem2.Name = "exitToolStripMenuItem2";
+            this.exitToolStripMenuItem2.Size = new System.Drawing.Size(216, 26);
+            this.exitToolStripMenuItem2.Text = "Exit";
+            // 
+            // outlookToolStripMenuItem
+            // 
+            this.outlookToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.newEmailToolStripMenuItem,
+            this.newTaskToolStripMenuItem2});
+            this.outlookToolStripMenuItem.Name = "outlookToolStripMenuItem";
+            this.outlookToolStripMenuItem.Size = new System.Drawing.Size(216, 26);
+            this.outlookToolStripMenuItem.Text = "Outlook";
+            // 
+            // newEmailToolStripMenuItem
+            // 
+            this.newEmailToolStripMenuItem.Name = "newEmailToolStripMenuItem";
+            this.newEmailToolStripMenuItem.Size = new System.Drawing.Size(216, 26);
+            this.newEmailToolStripMenuItem.Text = "New Email";
+            // 
+            // newTaskToolStripMenuItem2
+            // 
+            this.newTaskToolStripMenuItem2.Name = "newTaskToolStripMenuItem2";
+            this.newTaskToolStripMenuItem2.Size = new System.Drawing.Size(216, 26);
+            this.newTaskToolStripMenuItem2.Text = "New Task";
+            // 
+            // iNSERTToolStripMenuItem
+            // 
+            this.iNSERTToolStripMenuItem.Name = "iNSERTToolStripMenuItem";
+            this.iNSERTToolStripMenuItem.Size = new System.Drawing.Size(216, 26);
+            this.iNSERTToolStripMenuItem.Text = "INSERT";
+            // 
+            // uPDATEToolStripMenuItem
+            // 
+            this.uPDATEToolStripMenuItem.Name = "uPDATEToolStripMenuItem";
+            this.uPDATEToolStripMenuItem.Size = new System.Drawing.Size(216, 26);
+            this.uPDATEToolStripMenuItem.Text = "UPDATE";
+            // 
+            // dELETEToolStripMenuItem
+            // 
+            this.dELETEToolStripMenuItem.Name = "dELETEToolStripMenuItem";
+            this.dELETEToolStripMenuItem.Size = new System.Drawing.Size(216, 26);
+            this.dELETEToolStripMenuItem.Text = "DELETE";
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Image = ((System.Drawing.Image)(resources.GetObject("pictureBox1.Image")));
+            this.pictureBox1.Location = new System.Drawing.Point(965, 33);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(123, 65);
+            this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pictureBox1.TabIndex = 5;
+            this.pictureBox1.TabStop = false;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(326, 48);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(53, 17);
+            this.label1.TabIndex = 6;
+            this.label1.Text = "CustID:";
+            // 
+            // textBox2
+            // 
+            this.textBox2.Location = new System.Drawing.Point(381, 76);
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(100, 22);
+            this.textBox2.TabIndex = 7;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(312, 79);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(67, 17);
+            this.label2.TabIndex = 8;
+            this.label2.Text = "PartNum:";
+            // 
+            // dateTimePicker1
+            // 
+            this.dateTimePicker1.Format = System.Windows.Forms.DateTimePickerFormat.Short;
+            this.dateTimePicker1.Location = new System.Drawing.Point(580, 48);
+            this.dateTimePicker1.Name = "dateTimePicker1";
+            this.dateTimePicker1.Size = new System.Drawing.Size(112, 22);
+            this.dateTimePicker1.TabIndex = 9;
+            // 
+            // dateTimePicker2
+            // 
+            this.dateTimePicker2.Format = System.Windows.Forms.DateTimePickerFormat.Short;
+            this.dateTimePicker2.Location = new System.Drawing.Point(698, 48);
+            this.dateTimePicker2.Name = "dateTimePicker2";
+            this.dateTimePicker2.Size = new System.Drawing.Size(112, 22);
+            this.dateTimePicker2.TabIndex = 10;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(69, 48);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(41, 17);
+            this.label3.TabIndex = 11;
+            this.label3.Text = "BAQ:";
             // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(1100, 521);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.dateTimePicker2);
+            this.Controls.Add(this.dateTimePicker1);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.textBox2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.pictureBox1);
             this.Controls.Add(this.textBox1);
             this.Controls.Add(this.comboBox1);
             this.Controls.Add(this.dataGridView1);
@@ -133,6 +334,7 @@
             ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
             this.menuStrip1.ResumeLayout(false);
             this.menuStrip1.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
             this.ResumeLayout(false);
             this.PerformLayout();
 
@@ -149,6 +351,28 @@
         private System.Windows.Forms.ToolStripMenuItem optionsToolStripMenuItem;
         private System.Windows.Forms.ToolStripMenuItem resourcesToolStripMenuItem;
         private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.ToolStripMenuItem exportToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem excelToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem sendToToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem newTaskToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem saveToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem saveAsToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem exitToolStripMenuItem1;
+        private System.Windows.Forms.ToolStripMenuItem printPreviewToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem exitToolStripMenuItem2;
+        private System.Windows.Forms.ToolStripMenuItem iNSERTToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem uPDATEToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem dELETEToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem outlookToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem newEmailToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem newTaskToolStripMenuItem2;
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.DateTimePicker dateTimePicker1;
+        private System.Windows.Forms.DateTimePicker dateTimePicker2;
+        private System.Windows.Forms.Label label3;
     }
 }
 
--- Epicor/Epicor/Form1.cs
+++ Epicor/Epicor/Form1.cs
@@ -32,7 +32,7 @@ namespace Epicor
         private void button1_Click(object sender, EventArgs e)
         {
             Session epiSession;
-            epiSession = new Session("seth.romanowski", "Baseball33", "net.tcp://LW-EPICOR-APP/ERP102100", Session.LicenseType.Default, @"C:\Epicor\ERP10.2Client2\Client\config\ERP102100.sysconfig");
+            epiSession = new Session("seth.romanowski", "Baseball33", "net.tcp://LW-EPICOR-APP/ERP102200", Session.LicenseType.Default, @"C:\Epicor\ERP10.2Client\Client\config\ERP102200.sysconfig");
             ILauncher oTrans = null;
             if (epiSession != null)
             {
@@ -149,6 +149,53 @@ namespace Epicor
                         }
                     }
                 }
+                if (comboBox1.Text == "Part Material On-Hand")
+                {
+                    // Declare and Initialize Variables
+                    string BAQName = "PartMtlOnHand2";
+                    Ice.BO.QueryExecutionDataSet ds = new Ice.BO.QueryExecutionDataSet();
+
+                    // Add Parameter Rows
+                    // Definition: AddExecutionParameterRow(string ParameterID, string ParameterValue, string ValueType, bool IsEmpty, Guid SysRowID, string RowMod)
+                    // Possible ValueTypes: nvarchar, int, decimal, date, datetime, bit, uniqueidentifier, bigint
+                    // IsEmpty indicates if your passed value Is Empty because if it is, you can define in your params to use a default value if empty.
+                    // Typically you use string.IsNullOrEmpty(yourValueVariable) but if you are hard coding a value then you can simply set it to false
+                    ds.ExecutionParameter.AddExecutionParameterRow("PartNum", "1040980", "nvarchar", false, Guid.Empty, "A");
+
+                    // Use Business Object Directly
+                    Ice.Proxy.BO.DynamicQueryImpl dynamicQuery = WCFServiceSupport.CreateImpl<Ice.Proxy.BO.DynamicQueryImpl>((Ice.Core.Session)oTrans.Session, Epicor.ServiceModel.Channels.ImplBase<Ice.Contracts.DynamicQuerySvcContract>.UriPath);
+                    System.Data.DataSet results = dynamicQuery.ExecuteByID(BAQName, ds);
+
+                    string val, val2, val3, val4, val5, val6;
+                    string[] row;
+
+                    dataGridView1.ColumnCount = 6;
+                    dataGridView1.Columns[0].Name = "JobNum";
+                    dataGridView1.Columns[1].Name = "StartDate";
+                    dataGridView1.Columns[2].Name = "DueDate";
+                    dataGridView1.Columns[3].Name = "PartNum";
+                    dataGridView1.Columns[4].Name = "PartDesc";
+                    dataGridView1.Columns[5].Name = "ProdQty";
+
+
+                    // Lets Loop through our results
+                    if (results.Tables["Results"].Rows.Count > 0)
+                    {
+                        foreach (DataRow item in results.Tables["Results"].Rows)
+                        {
+                            // In E9 you used TableName.Column in E10 it is TableName_Column
+                            val = item["JobHead_JobNum"].ToString();
+                            val2 = item["JobHead_StartDate"].ToString();
+                            val3 = item["JobHead_DueDate"].ToString();
+                            val4 = item["JobHead_PartNum"].ToString();
+                            val5 = item["JobHead_PartDescription"].ToString();
+                            val6 = item["JobHead_ProdQty"].ToString();
+
+                            row = new string[] { val, val2, val3, val4, val5, val6 };
+                            dataGridView1.Rows.Add(row);
+                        }
+                    }
+                }
             }
             catch (System.Exception ex)
             {
@@ -156,7 +203,5 @@ namespace Epicor
             }
        
         }
-
-
     }
 }
